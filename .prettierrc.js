5ea676972071c97cab66e3a55e8e3ceb90657e5f
patrinoua <angela.patrinou@sharpist.com>
2021-07-30 23:52:12 +0200

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  semi: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "all",
+  useTabs: true,
+  endOfLine: "lf",
+  arrowParens: "avoid",
+};
